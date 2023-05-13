068514d2ec3aad12979a8398f08674ad837efef8
Ajeng Fitrihandini <ajengfitri1409@gmail.com>
2023-05-13 16:27:11 +0700

    ok selesai

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import "./Header.css";
+import "./navbar.css";
 import { Navbar, Nav, NavDropdown, Container, Button } from "react-bootstrap";
 import { useState, useEffect } from "react";
 import axios from "axios";
@@ -50,50 +50,95 @@ const Header = () => {
   }, [isLogin, isAdmin, jwtToken]);
 
   return (
-    <>
-      <Navbar expand="lg" >
-        <Container>
-          <Navbar.Brand className="header-brand " href="/">
-            <img className="logo-image" src={logo} alt="logo" style={{ width: "100px"}} />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto d-flex gap-3 ms-lg-5 mb-3 mb-lg-0">
-              <Nav.Link href="/" className="header-link">
-                Home
-              </Nav.Link>
-              {isLogin ? (
-                <Nav.Link href="/favorite" className="header-link">
-                  Favorite
-                </Nav.Link>
-              ) : null}
-              <Nav.Link href="/foods" className="header-link">
-                Recipes
-              </Nav.Link>
-              {isAdmin ? (
-                <Nav.Link href="/add-food" className="header-link">
-                  Add Food
-                </Nav.Link>
-              ) : null}
-            </Nav>
-            {isLogin ? (
-              <NavDropdown className="header-username" title={localStorage.getItem("name")} id="nav-dropdown">
-                <NavDropdown.Item href="/profile">Profile</NavDropdown.Item>
-                {isAdmin ? <NavDropdown.Item href="/all-user">All User (Admin)</NavDropdown.Item> : null}
-                <NavDropdown.Item onClick={handleLogout}>Logout</NavDropdown.Item>
-              </NavDropdown>
-            ) : (
-              <Button className="btn-signin">
-                <a href="/login" className="login-header">
-                Sign In
-              </a>
-              </Button>
+    <div className="header-container">
+  <Navbar expand="lg" className="w-100" collapseOnSelect={true}> 
+    <Container>
+      <Navbar.Brand className="header-brand " href="/">
+        <img className="logo-image" src={logo} alt="logo" style={{ width: "100px"}} />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" variant="dark"/> 
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto d-flex gap-3 ms-lg-5 mb-3 mb-lg-0">
+          <Nav.Link href="/" className="header-link">
+            Home
+          </Nav.Link>
+          {isLogin ? (
+            <Nav.Link href="/favorite" className="header-link">
+              Favorite
+            </Nav.Link>
+          ) : null}
+          <Nav.Link href="/foods" className="header-link">
+            Recipes
+          </Nav.Link>
+          {isAdmin ? (
+            <Nav.Link href="/add-food" className="header-link">
+              Add Food
+            </Nav.Link>
+          ) : null}
+        </Nav>
+        {isLogin ? (
+          <NavDropdown className="header-username" title={localStorage.getItem("name")} id="nav-dropdown">
+            <NavDropdown.Item href="/profile">Profile</NavDropdown.Item>
+            {isAdmin ? <NavDropdown.Item href="/all-user">All User (Admin)</NavDropdown.Item> : null}
+            <NavDropdown.Item onClick={handleLogout}>Logout</NavDropdown.Item>
+          </NavDropdown>
+        ) : (
+          <Button className="btn-signin">
+            <a href="/login" className="login-header">
+            Sign In
+          </a>
+          </Button>
+
+        )}
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+</div>
+
+    // <>
+    //   <Navbar expand="lg" >
+    //     <Container>
+    //       <Navbar.Brand className="header-brand " href="/">
+    //         <img className="logo-image" src={logo} alt="logo" style={{ width: "100px"}} />
+    //       </Navbar.Brand>
+    //       <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    //       <Navbar.Collapse id="basic-navbar-nav">
+    //         <Nav className="me-auto d-flex gap-3 ms-lg-5 mb-3 mb-lg-0">
+    //           <Nav.Link href="/" className="header-link">
+    //             Home
+    //           </Nav.Link>
+    //           {isLogin ? (
+    //             <Nav.Link href="/favorite" className="header-link">
+    //               Favorite
+    //             </Nav.Link>
+    //           ) : null}
+    //           <Nav.Link href="/foods" className="header-link">
+    //             Recipes
+    //           </Nav.Link>
+    //           {isAdmin ? (
+    //             <Nav.Link href="/add-food" className="header-link">
+    //               Add Food
+    //             </Nav.Link>
+    //           ) : null}
+    //         </Nav>
+    //         {isLogin ? (
+    //           <NavDropdown className="header-username" title={localStorage.getItem("name")} id="nav-dropdown">
+    //             <NavDropdown.Item href="/profile">Profile</NavDropdown.Item>
+    //             {isAdmin ? <NavDropdown.Item href="/all-user">All User (Admin)</NavDropdown.Item> : null}
+    //             <NavDropdown.Item onClick={handleLogout}>Logout</NavDropdown.Item>
+    //           </NavDropdown>
+    //         ) : (
+    //           <Button className="btn-signin">
+    //             <a href="/login" className="login-header">
+    //             Sign In
+    //           </a>
+    //           </Button>
               
-            )}
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </>
+    //         )}
+    //       </Navbar.Collapse>
+    //     </Container>
+    //   </Navbar>
+    // </>
   );
 };
 
--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -6,6 +6,9 @@ import { Container, Button, Row, Col } from "react-bootstrap";
 import { FaStar, FaBookmark, FaPlusCircle } from "react-icons/fa";
 import axios from "axios";
 import logo from "../../images/biofoodoke.png"
+import create  from "../../images/create.jpg"
+import healthy from "../../images/healthy.jpg"
+import review from "../../images/review.jpg"
 
 const LandingPage = () => {
   const [mostFavorite, setMostFavorite] = useState([]);
@@ -54,7 +57,7 @@ const LandingPage = () => {
             {/* <p className="intro-text mb-5">Look, cook, and enjoy.</p> */}
             <p className="intro-text mb-4">proud present</p>
 
-            <h2 className="intro-text mb-4">Review and Create Your Recipe</h2>
+            <h2 className="intro-text mb-4">Healthy, Create and Review</h2>
             <Button
               onClick={() => {
                 window.location.assign(`/foods`);
@@ -77,7 +80,7 @@ const LandingPage = () => {
       {/* Favorite Section */}
       {/* <div className="favorite-section pt-4 pb-5">
         <Container>
-          <h1 className="favorite-title mb-5 pt-3">Trending Recipes</h1>
+          <h1 className="favorite-title mb-5 pt-3">About Us</h1>
           <Row className="favorite-row">
             {mostFavorite.map((food, i) => {
               return (
@@ -100,41 +103,39 @@ const LandingPage = () => {
       {/* End of Favorite Section */}
 
       {/* Feature Section */}
-      {/* <div className="feature-section pt-5 pb-5">
+      <div className="feature-section pt-5 pb-5">
         <Container>
           <h1 className="feature-title mb-5">
             <span className="feature-title-brand">
-              <img className="logo-image" src={logo} alt="logo" />
+              <h1 className="favorite-title mb-5 pt-3">About Us</h1>
             </span>
-            <br />
-            
+          
           </h1>
           <Row>
             <Col md={12} lg={12} xl={4} className="d-flex flex-column align-items-center mb-md-4 mb-4">
-              <FaStar className="feature-icons mb-4" />
+              <img className="img-us" src={healthy} alt="logo" />
               <h1 className="feature-text">
-                Add
-                <br />
-                Rating & Review
+                
+                Healthy Recipes
               </h1>
             </Col>
             <Col sm={12} md={6} lg={6} xl={4} className="d-flex flex-column align-items-center mb-4">
-              <FaPlusCircle className="feature-icons mb-4" />
+              
               <h1 className="feature-text">
-                Add
-                <br /> New Recipe
+                <img className="img-us" src={create} alt="logo" />
+                <br /> Create Recipes
               </h1>
             </Col>
             <Col sm={12} md={6} lg={6} xl={4} className="d-flex flex-column align-items-center">
-              <FaBookmark className="feature-icons mb-4" />
+              
               <h1 className="feature-text">
-                Add
-                <br /> Favorite Food
+                <img className="img-us" src={review} alt="logo" />
+                <br /> Review Recipes
               </h1>
             </Col>
           </Row>
         </Container>
-      </div> */}
+      </div>
       {/* End of Feature Section */}
 
       {/* Footer Section */}
